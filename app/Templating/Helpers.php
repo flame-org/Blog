3b3e94b8b3e9648ad1f328c097a840eca1512aca
JSifalda <sifalda.jiri@gmail.com>
2013-02-15 20:03:49 +0100

    Remove helper md, add macro md

--- app/Templating/Helpers.php
+++ app/Templating/Helpers.php
@@ -10,9 +10,6 @@ namespace Flame\Blog\Templating;
 
 class Helpers extends \Nette\Object
 {
-	
-	/** @var \dflydev\markdown\MarkdownParser */
-	private static $markdownParser;
 
 	/**
 	 * @param $helper
@@ -23,28 +20,6 @@ class Helpers extends \Nette\Object
 		if (method_exists(__CLASS__, $helper)) {
 			return new \Nette\Callback(__CLASS__, $helper);
 		}
-	}
-
-	/**
-	 * @param $text
-	 * @return mixed
-	 */
-	public static function md($text)
-	{
-		$parser = static::getMakrdownParser();
-		return $parser->transformMarkdown($text);
-	}
-
-	/**
-	 * @return mixed
-	 */
-	private static function getMakrdownParser()
-	{
-		if(static::$markdownParser === null){
-			static::$markdownParser = new \dflydev\markdown\MarkdownParser;
-		}
-		
-		return static::$markdownParser;
 	}
 
 }
