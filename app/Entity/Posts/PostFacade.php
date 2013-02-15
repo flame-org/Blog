3233f52b31d4a5be18601736c85438eed72504f4
JSifalda <sifalda.jiri@gmail.com>
2013-02-15 14:57:08 +0100

    Add Posts entity

---
+++ app/Entity/Posts/PostFacade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PostFacade.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date    15.02.13
+ */
+
+namespace Flame\Blog\Entity\Posts;
+
+class PostFacade extends \Flame\Doctrine\Model\Facade
+{
+
+	protected $repositoryName = '\Flame\Blog\Entity\Posts\Post';
+
+}
